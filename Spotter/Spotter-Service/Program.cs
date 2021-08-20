acf4c776ce1305dddf3487b6b2116adb151ae95e
Oscar Davies <oscar.william.davies@outlook.com>
2021-08-20 15:59:04 +0100

    Init Service & API Project

---
+++ Spotter/Spotter-Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spotter_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
